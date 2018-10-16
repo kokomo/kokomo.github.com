44ac4a3785f89cc4b49e6c86d3a9eeec0f1e3f26
kokomo <illustopia@gmail.com>
2018-10-17 00:40:28 +0900

    modify

---
+++ js/index.20181017003716.js
@@ -0,0 +1,78 @@
+(function(d){var h=[];d.loadImages=function(a,e){"string"==typeof a&&(a=[a]);for(var f=a.length,g=0,b=0;b<f;b++){var c=document.createElement("img");c.onload=function(){g++;g==f&&d.isFunction(e)&&e()};c.src=a[b];h.push(c)}}})(window.jQuery);
+$.fn.hasAttr = function(name) { var attr = $(this).attr(name); return typeof attr !== typeof undefined && attr !== false; };
+
+
+$(document).ready(function() {
+r=function(){dpi=window.devicePixelRatio;$('.js7').attr('src', (dpi>1) ? 'images/mountains-1862-1.png' : 'images/mountains-931-1.png');
+$('.js9').attr('src', (dpi>1) ? ((dpi>2) ? 'images/flower_2-252.png' : 'images/flower_2-168-1.png') : 'images/flower_2-84-1.png');
+$('.js10').attr('src', (dpi>1) ? ((dpi>2) ? 'images/flower_1-252.png' : 'images/flower_1-168-1.png') : 'images/flower_1-84-1.png');
+$('.js11').attr('src', (dpi>1) ? ((dpi>2) ? 'images/flower_1-252.png' : 'images/flower_1-168-1.png') : 'images/flower_1-84-1.png');
+$('.js12').attr('src', (dpi>1) ? ((dpi>2) ? 'images/flower_2-252.png' : 'images/flower_2-168-1.png') : 'images/flower_2-84-1.png');
+$('.js13').attr('src', (dpi>1) ? ((dpi>2) ? 'images/flower_3-252.png' : 'images/flower_3-168-1.png') : 'images/flower_3-84-1.png');
+
+$('.js16').attr('src', (dpi>1) ? 'images/mountains_2-1910-1.png' : 'images/mountains_2-955-1.png');
+
+$('.js19').attr('src', (dpi>1) ? ((dpi>2) ? 'images/church-375.png' : 'images/church-250-1.png') : 'images/church-125-1.png');
+$('.js20').attr('src', (dpi>1) ? 'images/boyfriend_2-88-1.png' : 'images/boyfriend_2-44-1.png');
+$('.js21').attr('src', (dpi>1) ? 'images/boyfriend-88-1.png' : 'images/boyfriend-44-1.png');
+$('.js22').attr('src', (dpi>1) ? 'images/nube1-400-596.png' : 'images/nube1-400-298.png');
+$('.js25').attr('src', (dpi>1) ? ((dpi>2) ? 'images/music-198.png' : 'images/music-132-1.png') : 'images/music-66-1.png');
+$('.js26').attr('src', (dpi>1) ? 'images/house-220-1.png' : 'images/house-110-1.png');
+$('.js27').attr('src', (dpi>1) ? ((dpi>2) ? 'images/music_2-156.png' : 'images/music_2-104-1.png') : 'images/music_2-52-1.png');
+
+
+$('.js30').attr('src', (dpi>1) ? 'images/hotel-396-1.png' : 'images/hotel-198-1.png');
+$('.js31').attr('src', (dpi>1) ? 'images/nube1-400-544.png' : 'images/nube1-400-272.png');
+
+$('.js33').attr('src', (dpi>1) ? 'images/regalo-316-1.png' : 'images/regalo-158-1.png');
+$('.js34').attr('src', (dpi>1) ? ((dpi>2) ? 'images/regalo-315.png' : 'images/regalo-210-1.png') : 'images/regalo-105-1.png');
+$('.js35').attr('src', (dpi>1) ? ((dpi>2) ? 'images/regalo-186-1.png' : 'images/regalo-124-1.png') : 'images/regalo-62-1.png');
+$('.js36').attr('src', (dpi>1) ? ((dpi>2) ? 'images/regalo-237.png' : 'images/regalo-158-1.png') : 'images/regalo-79-1.png');
+$('.js37').attr('src', (dpi>1) ? ((dpi>2) ? 'images/confirm-1506.png' : 'images/confirm-1004-1.png') : 'images/confirm-502-1.png');};
+if(!window.HTMLPictureElement){r();}
+(function(){$('a[href^="#"]:not(.allowConsent,.noConsent,.denyConsent,.removeConsent)').each(function(){$(this).click(function(){var t=this.hash.length>1?$('[name="'+this.hash.slice(1)+'"]').offset().top:0;return $("html, body").animate({scrollTop:t},400),!1})})})();
+$('form').h5Validate();setTimeout(function(){$('.js38').prop('disabled',false);$("input[name='lqodobmi']").attr('value', 'bmhvqnufarsxqt');},1000);
+initMenu($('#m1')[0]);
+$('.c57').Stickyfill();
+var wl = new woolite();
+wl.init();
+wl.addAnimation($('.js')[0], "1.00s", "0.00s", 1, 100);
+wl.addAnimation($('.js2')[0], "1.00s", "0.00s", 1, 100);
+wl.addAnimation($('.js3')[0], "1.00s", "0.00s", 1, 100);
+wl.addAnimation($('.js4')[0], "1.00s", "0.00s", 1, 100);
+wl.addAnimation($('.js5')[0], "1.00s", "0.00s", 1, 100);
+wl.addAnimation($('.js6')[0], "1.00s", "0.00s", 1, 100);
+wl.addAnimation($('.js7')[0], "1.00s", "0.00s", 1, 100);
+wl.addAnimation($('.js8')[0], "1.00s", "0.00s", 1, 100);
+wl.addAnimation($('.js9')[0], "1.00s", "0.00s", 1, 100);
+wl.addAnimation($('.js10')[0], "1.00s", "0.00s", 1, 100);
+wl.addAnimation($('.js11')[0], "1.00s", "0.00s", 1, 100);
+wl.addAnimation($('.js12')[0], "1.00s", "0.00s", 1, 100);
+wl.addAnimation($('.js13')[0], "1.00s", "0.00s", 1, 100);
+wl.addAnimation($('.js14')[0], "1.00s", "0.00s", 1, 100);
+wl.addAnimation($('.js15')[0], "1.00s", "0.00s", 1, 100);
+wl.addAnimation($('.js16')[0], "1.00s", "0.00s", 1, 100);
+wl.addAnimation($('.js17')[0], "1.00s", "0.00s", 1, 100);
+wl.addAnimation($('.js18')[0], "1.00s", "0.00s", 1, 100);
+wl.addAnimation($('.js19')[0], "1.00s", "0.50s", 1, 100);
+wl.addAnimation($('.js20')[0], "1.00s", "0.50s", 1, 100);
+wl.addAnimation($('.js21')[0], "1.00s", "0.50s", 1, 100);
+wl.addAnimation($('.js22')[0], "1.00s", "0.00s", 1, 100);
+wl.addAnimation($('.js23')[0], "1.00s", "0.00s", 1, 100);
+wl.addAnimation($('.js24')[0], "1.00s", "0.00s", 1, 100);
+wl.addAnimation($('.js25')[0], "1.00s", "0.20s", 1, 100);
+wl.addAnimation($('.js26')[0], "1.00s", "0.00s", 1, 100);
+wl.addAnimation($('.js27')[0], "1.00s", "0.30s", 1, 100);
+wl.addAnimation($('.js28')[0], "1.00s", "0.00s", 1, 100);
+wl.addAnimation($('.js29')[0], "1.00s", "0.00s", 1, 100);
+wl.addAnimation($('.js30')[0], "1.00s", "0.00s", 1, 100);
+wl.addAnimation($('.js31')[0], "1.00s", "0.00s", 1, 100);
+wl.addAnimation($('.js32')[0], "1.00s", "0.00s", 1, 100);
+wl.addAnimation($('.js33')[0], "1.00s", "0.00s", 1, 100);
+wl.addAnimation($('.js34')[0], "1.00s", "0.00s", 1, 100);
+wl.addAnimation($('.js35')[0], "1.00s", "0.00s", 1, 100);
+wl.addAnimation($('.js36')[0], "1.00s", "0.00s", 1, 100);
+wl.addAnimation($('.js37')[0], "1.00s", "0.00s", 1, 100);
+wl.start();
+
+});
